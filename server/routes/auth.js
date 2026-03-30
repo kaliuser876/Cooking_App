0045cc3cfa8798cbec2f5b85105926c680a4a93d
kaliuser876 <jonrrich112@gmail.com>
2026-03-29 21:39:17 -0500

    Update

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   maxAge: 365 * 24 * 60 * 60 * 1000, // 1 year
 };
 
@@ -177,7 +177,7 @@ router.post("/logout", (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   });
   res.json({ message: "Logged out successfully." });
 });
